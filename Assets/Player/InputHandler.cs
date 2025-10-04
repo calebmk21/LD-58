9d013fa1ac54f069848c0b3939f04b2cae870761
Caleb Kim <calebmk21@gmail.com>
2025-10-03 20:37:51 -0700

    began collect theme woohoo

---
+++ Assets/Player/InputHandler.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
+public class InputHandler : MonoBehaviour
+{
+    public PlayerController controller;
+    
+    private InputAction _moveAction, _lookAction, _jumpAction, _sprintAction;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _lookAction = InputSystem.actions.FindAction("Look");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _sprintAction = InputSystem.actions.FindAction("Sprint");
+
+        _jumpAction.performed += OnJumpPerformed;
+        _sprintAction.performed += OnSprintPerformed;
+        
+        Cursor.visible = false;
+    }
+
+    // Passing inputs to CharacterController object
+    void Update()
+    {
+        Vector2 CharacterMovement = _moveAction.ReadValue<Vector2>();
+        controller.Move(CharacterMovement);
+
+        Vector2 SightlineVector = _lookAction.ReadValue<Vector2>();
+        controller.Look(SightlineVector);
+        
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        controller.Jump();
+    }
+
+    private void OnSprintPerformed(InputAction.CallbackContext context)
+    {
+        controller.Sprint();
+    }
+
+
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    // basic movement floats
+    public float moveSpeed = 10f, lookSpeed = 7f, jumpPower = 5f, gravityForce = -10f;
+    public float verticalVelocity;
+    public float rotY;
+
+    public bool isSprinting = false, isGrounded = true;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _controller.detectCollisions = true;
+    }
+
+    public void Move(Vector2 movement)
+    {
+        Vector3 move = transform.forward * movement.y + transform.right * movement.x;
+        move *= moveSpeed * Time.deltaTime;
+        _controller.Move(move);
+        verticalVelocity += gravityForce * Time.deltaTime;
+        Vector3 jumpVec = new Vector3(0, verticalVelocity, 0) * Time.deltaTime;
+        _controller.Move(jumpVec);
+
+        if (isGrounded)
+        {
+            verticalVelocity = 0f;
+        }
+    }
+
+    public void Look(Vector2 looking)
+    {
+        rotY += looking.x * lookSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(0, rotY, 0);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            verticalVelocity = jumpPower;
+            isGrounded = false;
+        }
+        else
+        {
+            Debug.Log("Not grounded, cannot jump");
+        }
+    }
+
+    public void Sprint()
+    {
+        if (!isSprinting)
+        {
+            Debug.Log("Sprinting");
+            moveSpeed *= 1.7f;
+            isSprinting = true;
+        }
+        else
+        {
+            Debug.Log("Not Sprinting");
+            moveSpeed /= 1.7f;
+            isSprinting = false;
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        
+        Debug.Log("Entered Collider " + other.gameObject.tag);
+        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            verticalVelocity = 0f;
+        }
+    }
+}
