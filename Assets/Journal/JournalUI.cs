ceb40be4fae37c14f55661d9e5affe0b38260724
Caleb Kim <calebmk21@gmail.com>
2025-10-05 09:40:53 -0700

    debugged journal

--- Assets/Journal/JournalUI.cs
+++ Assets/Journal/JournalUI.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class JournalUI : MonoBehaviour
 {
     public Journal journal;
-    public Dictionary<string, GameObject> progressionPanels;
+    public Dictionary<string, GameObject> progressionPanels =  new();
     //private int indexID = -1;
     void Start()
     { 
         GameObject[] progressionPanelArray = FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None).Where(sr => sr.CompareTag("Progression")).ToArray();
+        
+        //Debug.Log(progressionPanels);
+        // Debug.Log(progressionPanelArray[0].name);
+        // Debug.Log(progressionPanelArray[1].name);
+        // Debug.Log(progressionPanelArray[2].name);
+        
         for (int i = 0; i < progressionPanelArray.Length; i++)
         {
-            progressionPanels.Add(progressionPanelArray[i].name, progressionPanelArray[i]);
+            string panelName = progressionPanelArray[i].name;
+            GameObject obj = progressionPanelArray[i];
+            // Debug.Log("Adding prog panel " + panelName);
+            // Debug.Log("Adding panel " + obj);
+            progressionPanels.Add(panelName, obj);
         }
-        //Debug.Log(progressionPanels);
-        Debug.Log(progressionPanelArray[0].name);
-        Debug.Log(progressionPanelArray[1].name);
-        Debug.Log(progressionPanelArray[2].name);
+
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
